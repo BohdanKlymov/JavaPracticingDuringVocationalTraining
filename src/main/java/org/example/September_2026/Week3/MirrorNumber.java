f8fdf68450314bcd8c7198cea0bb26f4f8e3c982
BohdanKlymov <programmer.b.k.339@gmail.com>
2026-09-14 15:36:19 +0200

    finished last Recursive Call exercise

--- src/main/java/org/example/September_2026/Week3/MirrorNumber.java
+++ src/main/java/org/example/September_2026/Week3/MirrorNumber.java
@@ -9,4 +9,29 @@ public class MirrorNumber {
 //    welche die Spiegelzahl der übergebenen Zahl zurückgibt.
 //    Sie dürfen davon ausgehen, dass der Funktion nur natürliche Zahlen übergeben werden, zu denen
 //    eine Spiegelzahl existiert (d.h. die nicht mit einer 0 enden).
+
+    public static String MirrorNumBecomesBigger(int inputNum, String mirrorNumber) {
+        if (String.valueOf(inputNum).length() == mirrorNumber.length()) {
+            return mirrorNumber;
+        }
+        String inputNumForIndex = String.valueOf(inputNum);
+
+        int index = String.valueOf(inputNum).length() - 1 - mirrorNumber.length();
+
+        mirrorNumber += Character.getNumericValue(inputNumForIndex.charAt(index));
+
+        return MirrorNumBecomesBigger(inputNum, mirrorNumber);
+    }
+
+
+    public static String inputNumBecomesShorter(int inputNum, String mirrorNumber) {
+        if (inputNum == 0) {
+            return mirrorNumber;
+        }
+        String inputNumForIndex = String.valueOf(inputNum);
+
+        int index = String.valueOf(inputNum).length() - 1;
+
+        return inputNumBecomesShorter(inputNum / 10, mirrorNumber += Character.getNumericValue(inputNumForIndex.charAt(index)));
+    }
 }
--- src/test/java/org/example/September_2026/Week3/MirrorNumberTest.java
+++ src/test/java/org/example/September_2026/Week3/MirrorNumberTest.java
@@ -1,5 +1,40 @@
 package org.example.September_2026.Week3;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MirrorNumberTest {
 
+    @Test
+    void mirrorNumberOf13579ShouldBe97531() {
+        int input = 13579;
+        String expected = "97531";
+        String current = MirrorNumber.MirrorNumBecomesBigger(input, "");
+        assertEquals(expected, current);
+    }
+
+    @Test
+    void mirrorNumberOf2ShouldBe2() {
+        int input = 2;
+        String expected = "2";
+        String current = MirrorNumber.MirrorNumBecomesBigger(input, "");
+        assertEquals(expected, current);
+    }
+
+    @Test
+    void mirrorNumberOf6892ShouldBe2986() {
+        int input = 6892;
+        String expected = "2986";
+        String current = MirrorNumber.inputNumBecomesShorter(input, "");
+        assertEquals(expected, current);
+    }
+
+    @Test
+    void mirrorNumberOf8ShouldBe8() {
+        int input = 8;
+        String expected = "8";
+        String current = MirrorNumber.inputNumBecomesShorter(input, "");
+        assertEquals(expected, current);
+    }
 }
